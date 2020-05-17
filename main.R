80cec3e04565d4487cb9d5e4e6396011872467a4
Sander Timmer <sandertimmer@gmail.com>
2020-05-17 12:37:46 +0200

    First MinMax version

---
+++ main.R
@@ -0,0 +1,16 @@
+library(tercen)
+library(dplyr)
+
+
+min_max_norm <- function(x) {
+    (x - min(x)) / (max(x) - min(x))
+  }
+
+#apply Min-Max normalization
+
+(ctx = tercenCtx())  %>% 
+  select(.y, .ci, .ri) %>% 
+  group_by(.ci, .ri) %>%
+  summarise(mean = min_max_norm(.y)) %>%
+  ctx$addNamespace() %>%
+  ctx$save()
